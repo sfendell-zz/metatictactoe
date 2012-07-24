9877b44088bff6d890f22f9f0ac3f8638522f8a1
Samuel P. Fendell <sfendell@berkeley.edu>
2012-07-23 21:20:24 -0700

    tuples instead
    changed to a tuple representation to describe the coordinates of a board

--- metattt.py
+++ metattt.py
@@ -7,31 +7,32 @@ def nextPlayer(player):
 class Board(object):
 	"""represents a n-dimensional board object"""
 	
-	def __init__(self, dimension, size, parent=None, row = None, col = None):
+	def __init__(self, dimension, size, parent=None, pos=None):
 		self.dimension = dimension
 		self.size = size
 		self.state = -1
 		self.parent = parent
-		self.col = col
-		self.row = row
+		self.pos = pos
 		if dimension>0:
-			self.children = [[Board(dimension-1,size,self,row,col) for col in range(size)] for row in range(size)]
+			self.children = [[Board(dimension-1,size,self,(row,col)) for col in range(size)] for row in range(size)]
 			
 	"""Takes in player (one letter from the player array) and 2*dimension arguments representing a 0-d square on the board, places the 
 	correct marking there, then updates all the children's states correctly and checks if that move wins the game"""
 	def move(self, player, *args):
-		if len(args)!=2*self.dimension:
-			raise Exception('move has wrong number of indices: should be %s, is %s' % (self.dimension*2, len(args))) #should never be thrown
+		if len(args)!=self.dimension:
+			raise Exception('move has wrong number of indices: should be %s, is %s' % (self.dimension, len(args))) #should never be thrown
 		if self.state != -1:
 			raise Exception('square of dimension %s has already been won!' % self.dimension)
 		if self.dimension==0:
 			self.state=player
 			self.parent.checkwin(player,self.row,self.col)
 		else:
-			self.children[args[0]][args[1]].move(player,*args[2:])
+			self.children[args[0][0]][args[0][1]].move(player,*args[1:])
 			
 	"""After player wins the (row, col) box of the board, check if the board has been won, set state appropriately, and alert its parent"""
-	def checkwin(self,player,row,col):
+	def checkwin(self,player,coord):
+		row = coord[0]
+		col = coord[1]
 		children = self.children
 		parent = self.parent
 		size = self.size
@@ -46,8 +47,8 @@ class Board(object):
 			parent.checkwin(player,self.row,self.col)
 
 	def genrule(self, *args)
-		if len(args)!=2*self.dimension:
-			raise Exception('move has wrong number of indices: should be %s, is %s' % (self.dimension*2, len(args))) #should never be thrown
+		if len(args)!=self.dimension:
+			raise Exception('move has wrong number of indices: should be %s, is %s' % (self.dimension, len(args))) #should never be thrown
 		if self.children(
 
 			
